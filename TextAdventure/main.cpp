49b4fe57801d6d195c2d7e9d05cfc750faf25aca
jackskis <jackzellweger1@gmail.com>
2014-01-24 14:45:55 -0500

    Text Adventure
    This is a text adventure by Ben Marx & Jack Zellweger at Kenyon College.
    The year is 2014..

---
+++ TextAdventure/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TextAdventure
+//
+//  Created by Jack Zellweger on 1/24/14.
+//  Copyright (c) 2014 Jack Zellweger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
