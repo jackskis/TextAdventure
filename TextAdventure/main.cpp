0f25db3cd16e7e03ee29c7d3b1ec984c4e28da6f
jackskis <jackzellweger1@gmail.com>
2014-02-06 23:13:00 -0500

    Debug + 1 add
    Added one more step at end/debugged
    Runs fine as it is.

--- TextAdventure/main.cpp
+++ TextAdventure/main.cpp
@@ -19,6 +19,8 @@
 #include <cmath>
 using namespace std;
 
+int nonlazy = 0, box;
+
 int main()
 {
     string player_name;
@@ -27,36 +29,51 @@ int main()
     string username, last_name;
     cin >> username >> last_name;
     
-    cout << "Hello, " << username << " " << last_name << ". I am pleased to meet you! \nGet ready for the best text adventure you've \never been on!" << endl;
+    cout << endl << "Hello, " << username << " " << last_name << ". I am pleased to meet you!\n" << endl << "Get ready for the best text adventure you've \never been on!" << endl;
     
-    cin.get()
+    cin.get();
     cout << "You awake on the floor of a small, very dimly lit room. \nYou don't remeber how you got here. \nThe only light is coming from a single bulb hanging from the ceiling.\nThere is a  door to your left. \n There is a small wooden box lying on the floor to your right. \n" << endl;
-    cout << "Do you want to try to open the door?\nPress 1 for YES and 2 for NO" << endl;
-    cin >> door;
-    if (nonlazy == 0)
+    cout << "Do you want to try to open the box?\nPress 1 for YES and 2 for NO" << endl;
+    cin >> box;
+    while( (box !=1) && (box !=2))
     {
-        switch(door)
+        cout << "That's not a valid answer\nPlease re-enter your answer: ";
+        cin >> box;
+    }
+        switch(box)
         {
     
         case 1:
-                cout << "You attempt to open the door, it won't open." << endl << endl;
+                cout << "\nYou attempt to open the box, it won't open." << endl << endl;
                 nonlazy--;
             break;
         case 2:
                 cout << "You sit lazily on your bum " << endl << endl;
         }
-    }
-    if(nonlazy == 1)
-    {
-         cout << "Well, the door won't open, and your stuck in a dark room...\n";
-    }else;
     
-    cout << "You, " << username << " begin to panic, looking around frantically for a \nway out. You spot a key..." << "Do you want to pick up the key? \nPress 1 for YES and 2 for NO";
-    int response1;
-    if (response1 == '1')
+    cout << "You, " << username << " begin to panic, looking around frantically for a \nway out. You spot a key..." << "\n\nDo you want to pick up the key? \nPress 1 for YES and 2 for NO\n";
+    
+    int response1 = 2;
+    
+    while (response1 == 2)
+    {
+    
+    cin >> response1;
+        switch(response1)
         {
-            cout << "You pick up the key";
-        };
+            case 1:
+                cout << "The key is rusty and cold as you pick it up. You...\n\n"; //add something CREATIVE here!!
+                    t
+                
+                break;
+            case 2:
+            {
+                cout << "You sit lazily on your bum\n\n";
+                cout << "You hear a clock begin ticking, looks like time might be running out"
+                response1 = 2;
+            }
+        }
+    }
     
     cout << "\n";
     
