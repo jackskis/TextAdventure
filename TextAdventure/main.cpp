912ad5968d71340a6447e06626a84a3f80c486be
Jack Zellweger <jackzellweger1@gmail.com>
2014-05-19 15:37:20 -0700

    Corecftreda
    fhkjsa

--- TextAdventure/main.cpp
+++ TextAdventure/main.cpp
@@ -23,9 +23,9 @@ int main()
    
     int door, nonlazy = 0;
    
-    cout << "You awake on the floor of a small, very dimly lit room."
-         << "You don't remeber how you got here. \nThe only light is coming from a single bulb hanging from the ceiling." 
-         << "There is a  door to your left."
+    cout << "You awake on the floor of a small, very dimly lit room." << endl
+         << "You don't remeber how you got here. \nThe only light is coming from a single bulb hanging from the ceiling." << endl
+         << "There is a  door to your left." << endl
          << endl;
     cout << "Do you want to try to open the door?\nPress 1 for YES and 2 for NO"
          << endl;
@@ -48,12 +48,20 @@ int main()
          cout << "Well, the door won't open, and your stuck in a dark room...\n";
     }else;
     
+    char response2;
+    
     cout << "You, "
          << username
-         << " begin to panic, looking around frantically for a"
-         << "way out. You spot a key..."
+         << " begin to panic, looking around frantically for a" << endl
+         << "way out. You spot a key..." << endl
          << "Do you want to pick up the key? \nPress 1 for YES and 2 for NO";
          << endl;
+         
+         /*response2 = */cin.get(response2);
+         //Does this work??
+         
+         
+         
 return 0;
     
 
